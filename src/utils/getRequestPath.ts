328cdef323074c72ddb08d90c7510efc99c9022e
SuperMadMel <supermadmel@gmail.com>
2025-09-30 00:17:42 +1000

    add request

--- src/utils/getRequestPath.ts
+++ src/utils/getRequestPath.ts
@@ -1,5 +1,6 @@
 export const getRequestPath = () => {
-	const requestPath = `http://localhost:3000`
+	// const requestPath = `http://localhost:3000`
+	const requestPath = `https://test-task-001.vercel.app/`
 
 	return {
 		requestPath,
