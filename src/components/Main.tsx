b09443e657c6e38a8702980b6054f450832cfbbe
SuperMadMel <supermadmel@gmail.com>
2025-09-30 01:02:00 +1000

    Changed Main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,10 @@ export default function Main() {
 		<div className='p-5 mx-auto'>
 			<h1 className='text-3xl font-bold mb-4 text-center'>Test Task 001</h1>
 			<p className='mb-3 text-center'>
-				Это Блок Main, для которого я не придумал наполнение.
+				JSONPLACEHOLDER сегодня по какой-то причине недоступен. Для страницы CSR
+				сделал API постов, но загрузка получается долгой, потому что сначала
+				идёт запрос к jsonplaceholder, и только после неудачи — запрос идет к
+				внутреннему API. Поэтому требуется немного подождать.
 			</p>
 		</div>
 	)
